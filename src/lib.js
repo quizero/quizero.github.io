dd3c112853e42521ab091417f435e0a469c6de31
Zhivko76 <zkzhelyazkov@gmail.com>
2021-03-25 11:59:54 +0200

    Added question editor functionality

---
+++ src/lib.js
@@ -0,0 +1,11 @@
+// import page from '../node_modules/page/page.mjs';
+// import { render } from '../node_modules/lit-html/lit-html.js';
+
+import page from '//unpkg.com/page/page.mjs';
+import { html, render } from '//unpkg.com/lit-html?module';
+
+export {
+    page,
+    html,
+    render
+};
